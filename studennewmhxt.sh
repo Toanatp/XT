758ec4430bcda1dd7e5debba187a75d1127cbfad
Toanatp <101535392+Toanatp@users.noreply.github.com>
2022-03-15 12:21:43 +0800

    Update studennewmhxt.sh

--- studennewmhxt.sh
+++ studennewmhxt.sh
@@ -19,7 +19,7 @@ do
   echo "Start vps lan $x"
   az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
-  az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
+  az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/Toanatp/XT/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
   for vps in switzerlandnorth southeastasia eastus eastus2 westus2 northeurope westeurope australiaeast centralindia uksouth koreacentral francecentral
   do
     if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
